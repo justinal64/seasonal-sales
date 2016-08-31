13f7115504cc10be6bb3b285d82398323d8b9924
justin leggett <justin@codingrookie.com>
2016-08-31 13:44:20 -0500

    completed the project.

--- js/seasonal-sales.js
+++ js/seasonal-sales.js
@@ -1,88 +1,81 @@
 var categories = {};
 var products = {};
+var discountSelect = document.getElementById("discount");
 
 document.getElementById("discount").addEventListener("change", function() {
     var discountValue = this.value;
-    var discountSeason = this;
-    console.log(discountSeason);
-    applyDiscount(discountValue);
+    // Selects the text IE. Winter
+    var discountSeason = this.options[this.selectedIndex].text;
+    if(discountSeason === "Winter") {
+      discountSeason = 1;
+    } else if(discountSeason === "Autumn") {
+      discountSeason = 2;
+    } else {
+      discountSeason = 3;
+    }
+    applyDiscount(discountValue, discountSeason);
 })
 
 function catArray() {
-    var categories = JSON.parse(this.responseText);
-    console.log("categories = ", categories);
-    // console.log("catArray data", categories.categories[0].id);
-    var discountSelect = document.getElementById("discount");
+    categories = JSON.parse(this.responseText);
     var discount = "<option value=''></value>";
-
-    for(var i = 0; i < 3; i++) {
-        discount += `<option value="${(categories.categories[i].discount * 100)}">${categories.categories[i].season_discount}</option>`;
+    for(var i = 0; i < categories.categories.length; i++) {
+        discount += `<option value="${(categories.categories[i].discount)}">${categories.categories[i].season_discount}</option>`;
     }
     discountSelect.innerHTML += discount;
-
 }
 
-function applyDiscount(discount) {
-    // Logic to apply discount
-    console.log("The discount is ", discount, "%");
+function applyDiscount(discount, season) {
+    // loop over array and apply the discount
+    for(var i = 0; i < products.products.length; i++) {
+        if(products.products[i].category_id === season) {
+                                            // Limits the decimal value to 2 places
+            products.products[i].price = (products.products[i].price * (1.0 - discount)).toFixed(2);
+        }
+    }
+    displayArray();
 }
 
 function onError() {
-  console.log("An error occurred while transferring");
-}
-
-function onProgress() {
-    // console.log("onProgress working");
+    alert("An error occurred while transferring");
 }
 
 function onLoad() {
-  var products = JSON.parse(this.responseText);
-  console.log("data", products);
+    products = JSON.parse(this.responseText);
+    displayArray();
+}
 
-  var productDiv = document.getElementById("products");
+function displayArray() {
+    var productDiv = document.getElementById("products");
+    var product = "";
+    var currentProduct;
 
-
-  var product = "";
-  var currentProduct;
-
-  for (var i = 0; i < 10; i++) {
-    currentProduct = products["products"][i];
-    // console.log("currentProduct price = ", currentProduct.price);
-    product += "<div class='products'>";
-    product += `<p>${currentProduct.name} ${currentProduct.price}`;
-    // Better solution?????
-    if(currentProduct.category_id === 1) {
-        product += " Winter</p>";
-    } else if (currentProduct.category_id === 2) {
-        product += " Furniture</p>";
-    } else {
-        product += " Household</p>";
-    }
-
-    //   product += "</div>";
-    //   product += `<img class='album' src=${currentProduct.icon_url} width="200px" height="200px">`;
-        // product += currentSong.icon_url;
-      // product += "</img>";
-    product += "</div>";
-  };
-
-
-  productDiv.innerHTML += product;
+    for (var i = 0; i < products.products.length; i++) {
+        currentProduct = products["products"][i];
+        // console.log("currentProduct price = ", currentProduct.price);
+        product += "<div class='products'>";
+        product += `<p>${currentProduct.name} ${currentProduct.price}`;
+        // Better solution?????
+        if(currentProduct.category_id === 1) {
+            product += " Winter</p>";
+        } else if (currentProduct.category_id === 2) {
+            product += " Furniture</p>";
+        } else {
+            product += " Household</p>";
+        }
+        product += "</div>";
+    };
+  productDiv.innerHTML = product;
 }
 
 var productsRequest = new XMLHttpRequest();
-
 productsRequest.addEventListener("load", onLoad); //Callback
 productsRequest.addEventListener("error", onError)
-productsRequest.addEventListener("progress", onProgress)
 productsRequest.open("GET", "products.json")
 productsRequest.send();
 
-
 var categoriesRequest = new XMLHttpRequest();
-
 categoriesRequest.addEventListener("load", catArray); //Callback
 categoriesRequest.addEventListener("error", onError)
-categoriesRequest.addEventListener("progress", onProgress)
 categoriesRequest.open("GET", "categories.json")
 categoriesRequest.send();
